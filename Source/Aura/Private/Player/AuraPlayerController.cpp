ec24232f5b7c42ef2470be3d6bb8206c0ed643f7
Taha KARABIYIK <tahakrb275@gmail.com>
2024-04-02 04:06:51 +0300

    Game Mode Base, Move, Highlight

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,13 +2,21 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "Interaction/EnemyInterface.h"
 
 #include "EnhancedInputSubsystems.h"
-
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true; 
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -27,4 +35,93 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>()) 
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor()); // if the actor is not an enemy interface returns nullptr, other case implement it
+
+	/**
+	* Line trace from cursor. The are severeal scenarios:
+	*
+	*	A. LastActor is null && ThisActor is null
+	*		- Do Nothing
+	*
+	*	B. LastActor is null && ThisActor is valid
+	*		- Highlight ThisActor
+	*
+	*	C. LastActor is valid && ThisActor is null
+	*		- UnHighlight LastActor
+	*
+	*	D. Both actors are valid, but LastActor != ThisActor
+	*		- UnHighlight LastActor and  Highlight ThisActor
+	*
+	*	E. Both actors are valid, and are the same actor
+	*		- Do Nothing
+
+	*/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr) {
+
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else 
+		{
+			// Case A - both are null, do nothing 	
+		}
+
+	}
+	else // LastActor is valid 
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C 
+			LastActor->UnHighlightActor();
+		}
+		else // both  actors are valid 
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D 
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else {
+				// Case E - do nothing
+			}
+		}
+	}
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -7,6 +7,10 @@
 #include "AuraPlayerController.generated.h"
 
 class UInputMappingContext;
+class UInputAction;
+class IEnemyInterface;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -17,12 +21,25 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
+
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void SetupInputComponent() override;
 private:
+
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
 
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
+	
+	
 };
